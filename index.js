db182273b88dc07b1017717ad13ffa034bbde3ac
RGGOLIK <152860170+RggolikSN@users.noreply.github.com>
2025-10-20 18:51:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function home() {
+    document.location.href = 'https://rggoliksn.github.io/RGG-LIDE/'
+}
 
+function info() {
+    document.location.href = 'https://rggoliksn.github.io/RGG-LIDE/info/'
+}
+
+function social() {
+    document.location.href = 'https://rggoliksn.github.io/RGG-LIDE/social/'
+}
+
+function top1() {
+    document.location.href = 'https://vk.com/audio-2001922006_135922006'
+}
+
+function top2() {
+    document.location.href = 'https://vk.com/audio-2001503180_133503180'
+}
+
+function top3() {
+    document.location.href = 'https://vk.com/audio-2001753471_141753471'
+}
