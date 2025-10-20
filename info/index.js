e93e62c22790ce69d43a2f0fe7cf88be9b871218
RGGOLIK <152860170+RggolikSN@users.noreply.github.com>
2025-10-20 19:13:20 +0300

    Update index.js

--- info/index.js
+++ info/index.js
@@ -9,3 +9,27 @@ function info() {
 function social() {
     document.location.href = 'https://rggoliksn.github.io/RGG-LIDE/social/'
 }
+
+function tw() {
+    document.location.href = 'https://www.twitch.tv/vexikoff'
+}
+
+function pr() {
+    document.location.href = 'https://t.me/VEXIPRIVATEBOT'
+}
+
+function tg() {
+    document.location.href = 'https://t.me/VeXikoff'
+}
+
+function dt() {
+    document.location.href = 'https://www.donationalerts.com/r/rggoliks'
+}
+
+function yt() {
+    document.location.href = 'https://www.youtube.com/@rggol1k'
+}
+
+function st() {
+    document.location.href = 'https://steamcommunity.com/id/VeXikix/'
+}
