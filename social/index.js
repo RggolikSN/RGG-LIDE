e0418d23d1e3181f106abaa71bb0f556b7073637
RGGOLIK <152860170+RggolikSN@users.noreply.github.com>
2025-10-20 18:57:36 +0300

    Create index.js

---
+++ social/index.js
@@ -0,0 +1,11 @@
+function home() {
+    document.location.href = 'https://rggoliksn.github.io/RGG-LIDE/'
+}
+
+function info() {
+    document.location.href = 'https://rggoliksn.github.io/RGG-LIDE/info/'
+}
+
+function social() {
+    document.location.href = 'https://rggoliksn.github.io/RGG-LIDE/social/'
+}
